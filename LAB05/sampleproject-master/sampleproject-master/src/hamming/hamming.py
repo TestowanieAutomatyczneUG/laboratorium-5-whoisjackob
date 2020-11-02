2a834f16fbdb6dc0b09a3725992ac2fe681e1945
jszczepanski <jackobgfx@gmail.com>
2020-11-02 19:04:41 +0100

    count different index

--- LAB05/sampleproject-master/sampleproject-master/src/hamming/hamming.py
+++ LAB05/sampleproject-master/sampleproject-master/src/hamming/hamming.py
@@ -6,6 +6,29 @@ class hamming:
 
             return 0
 
+        elif len(input1) >= 5 or len(input2) >= 5:
+
+            count = 0
+            k = 0
+
+            def check(count, input1, input2, k):
+
+                if k == len(input1):
+                    return count
+                else:
+                    if input1[k] != input2[k]:
+                        check(count + 1, input1, input2, k + 1)
+                    else:
+                        check(count, input1, input2, k + 1)
+
+            check(count, input1, input2, k)
+
         elif input1 == input2:
 
             return 0
+
+        elif input1 != input2:
+
+            return 1
+
+
