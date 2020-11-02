7c00ec282bb898792b181889e3fc2946f991bd4e
jszczepanski <jackobgfx@gmail.com>
2020-11-02 20:29:38 +0100

    fourth test passed

--- LAB05/sampleproject-master/sampleproject-master/src/roman/roman.py
+++ LAB05/sampleproject-master/sampleproject-master/src/roman/roman.py
@@ -1,4 +1,10 @@
 
 def roman(input1):
-    input2 = input1*"I"
-    return input2
+
+    if input1 == 4:
+        input2 = (input1-3) * "I" + "V"
+        return input2
+
+    else:
+        input2 = input1*"I"
+        return input2
