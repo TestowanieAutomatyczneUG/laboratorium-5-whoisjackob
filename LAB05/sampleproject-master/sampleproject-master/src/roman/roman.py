1923bdb2e4cd9a8810ac759c61165607486daea6
jszczepanski <jackobgfx@gmail.com>
2020-11-02 20:38:14 +0100

    fifth test passed

--- LAB05/sampleproject-master/sampleproject-master/src/roman/roman.py
+++ LAB05/sampleproject-master/sampleproject-master/src/roman/roman.py
@@ -5,6 +5,10 @@ def roman(input1):
         input2 = (input1-3) * "I" + "V"
         return input2
 
+    elif input1 == 5:
+        input2 = "V"
+        return input2
+
     else:
         input2 = input1*"I"
         return input2
